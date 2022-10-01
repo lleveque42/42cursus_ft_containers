c91966b39244ead0db2304ed4ec352f977863b69
lleveque42 <louis.leveque@outlook.com>
2022-10-01 02:10:54 +0200

    main update

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,24 +5,22 @@
 int main(void) {
 
 	ft::vector<int> my(10, 2);
-	std::vector<int> og(10, 2);
+	std::vector<int> og(13, 2);
 
 	my.print();
 	for (size_t i = 0; i < og.size(); i++)
 		std::cout << "og[" << i << "]" << og[i] << std::endl;
 	try
 	{
-		my.assign(10, 21);
+		std::cout << my.at(32) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	my.print();
-	std::cout << my.size() << std::endl;
 	try
 	{
-		og.assign(10, 21);
+		std::cout << og.at(32) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
